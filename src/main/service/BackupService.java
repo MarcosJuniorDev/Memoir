f8323b43b34a6d32bc205bfbe02d9e3a1041ad1d
MarcosjuniorDev <marcosjoliveiradev@gmail.com>
2026-07-18 02:31:48 -0300

    fix: nome de pasta de backup com ":"  no Windows

--- src/main/service/BackupService.java
+++ src/main/service/BackupService.java
@@ -43,14 +43,23 @@ public class BackupService {
         this.gameRepository = gameRepository;
     }
 
+    //HELPER NECESSARIO PORQUE A PORRA DO WINDOWS NAO DEIXA SALVAR PASTA COM ":"
+    //E EM UM EXEMPLO BATMAN: ARKHAM ELE QUEBRA A APLICACAO
+    private String getSafeBackupFolderName(){
+        return game.getName()
+                .replaceAll("[\\\\/:*?\"<>|]", "").trim();
+    }
+
     public void ensureBackupDirecotry()throws IOException {
-        if (!Files.exists(Paths.get(game.getBackupLocation(), game.getName()))){
-            Files.createDirectories(Paths.get(game.getBackupLocation(), game.getName()));
+        if (!Files.exists(Paths.get(game.getBackupLocation(), getSafeBackupFolderName()))){
+            Files.createDirectories(Paths.get(game.getBackupLocation(), getSafeBackupFolderName()));
         }
     }
 
     public void deleteBackupFile(String backupPath, String folderName) throws IOException{
-        FileUtils.deleteDirectory(new File(backupPath, folderName));
+        //AJUSTE PRO WINDOWS
+        String safeFolderName = folderName.replaceAll("[\\\\/:*?\"<>|]", "").trim();
+        FileUtils.deleteDirectory(new File(backupPath, safeFolderName));
     }
 
 
@@ -58,11 +67,11 @@ public class BackupService {
         return hashFileSave.SaveFileHash(Paths.get(game.getSaveGamePath()));
     }
     public String verifyHashBackupFolder() throws NoSuchAlgorithmException{
-        return hashFileSave.SaveFileHash(Paths.get(game.getBackupLocation(),  game.getName()));
+        return hashFileSave.SaveFileHash(Paths.get(game.getBackupLocation(),  getSafeBackupFolderName()));
     }
 
     public boolean isBackupNeeded(String currentHash){
-        if (!Files.exists(Paths.get(game.getBackupLocation(), game.getName()))){
+        if (!Files.exists(Paths.get(game.getBackupLocation(), getSafeBackupFolderName()))){
             return true;
         }
         String lasthash = game.getLastHash();
@@ -71,7 +80,7 @@ public class BackupService {
 
     public boolean gameBackupFolderExist(){
 
-        return Files.exists(Paths.get(game.getBackupLocation(), game.getName()));
+        return Files.exists(Paths.get(game.getBackupLocation(), getSafeBackupFolderName()));
     }
 
     public boolean saveFilesBackup() throws Exception{
@@ -79,7 +88,7 @@ public class BackupService {
         if (isBackupNeeded(currentHash)) {
             System.out.println("backup realizado");
             ensureBackupDirecotry();
-            FileUtils.copyDirectory(Paths.get(game.getSaveGamePath()).toFile(), Paths.get(game.getBackupLocation(), game.getName()).toFile());
+            FileUtils.copyDirectory(Paths.get(game.getSaveGamePath()).toFile(), Paths.get(game.getBackupLocation(), getSafeBackupFolderName()).toFile());
             game.setLastHash(currentHash);
             game.setLastBackup();
             gameRepository.update(game);
@@ -91,7 +100,7 @@ public class BackupService {
 
    public boolean restoreBackup() throws IOException {
         if (gameBackupFolderExist()){
-            FileUtils.copyDirectory(Paths.get(game.getBackupLocation(), game.getName()).toFile(), Paths.get(game.getSaveGamePath()).toFile());
+            FileUtils.copyDirectory(Paths.get(game.getBackupLocation(), getSafeBackupFolderName()).toFile(), Paths.get(game.getSaveGamePath()).toFile());
             return true;
         }
         else {
